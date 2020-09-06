9a39f8bfc57618c13d1d27df70f05aad93656fbf
alansun17904 <asun17904@gmail.com>
2020-09-06 02:53:34 -0400

    implemented hashing for processing directories

--- app/application.py
+++ app/application.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 import threading
 from flask import Flask, session, render_template, request, url_for, jsonify, redirect, flash
 from flask_cors import CORS
@@ -161,24 +162,26 @@ def upload_video():
                 video_id = 0
             else:
                 video_id = video_id[0] + 1
-            parent_dir = Path(app.config['UPLOAD_FOLDER']) / f'video{video_id}'
+            hash_string = session['username'] + str(video_id) + datetime.now().strftime('%Y-%m-%d')
+            hashed = hashlib.sha1(bytes(hash_string, 'utf-8')).hexdigest() 
+            parent_dir = Path(app.config['UPLOAD_FOLDER']) / hashed 
             os.mkdir(parent_dir)
             filename = secure_filename(file.filename)
-            save_directory = str(Path(f'video{video_id}') / filename)
+            save_directory = str(Path(hashed) / filename)
             file.save(Path(app.config['UPLOAD_FOLDER']) / save_directory)
             # adding video into the database for later
             print(request.form['selected_physician'])
             db.execute("INSERT INTO videos (patient_id, filepath_old, filepath_new, physician_id, date, status, name)" +
                         " VALUES (:patient_id, :filepath_old, :filepath_new, :physician_id, :date, :status, :name)",
                         {"patient_id": user_info[0], "filepath_old": save_directory,
-                         "filepath_new": str(Path(f'video{video_id}') / ('high_res_' + filename)),
+                         "filepath_new": str(Path(hashed) / ('high_res_' + filename)),
                          "physician_id": request.form['selected_physician'],
                          "date": datetime.now().strftime('%Y-%m-%d'), "status": "processing",
                          "name": request.form['title']})
             db.commit()
             # increase_res(str(Path(app.config['UPLOAD_FOLDER']) / save_directory), f'video{video_id}', 'high_res' + filename, video_id)
             process_video = threading.Thread(target=increase_res,
-                                             args=(str(Path(app.config['UPLOAD_FOLDER']) / save_directory), f'video{video_id}', 'high_res_' + filename, video_id), daemon=True)
+                                             args=(str(Path(app.config['UPLOAD_FOLDER']) / save_directory), hashed, 'high_res_' + filename, video_id), daemon=True)
             process_video.start()
             return redirect(url_for("dashboard"))
     else:
